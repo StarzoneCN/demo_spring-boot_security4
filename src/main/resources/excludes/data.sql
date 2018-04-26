bd708cfd1792c80c3dae1aae6ad5029f681647e1
LiHongxing <lihongxing@bluemoon.com.cn>
2018-04-26 10:47:06 +0800

    no message

--- src/main/resources/excludes/data.sql
+++ src/main/resources/excludes/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO `spring_security_test`.`user`(`username`, `password`) VALUES ('admin', '$2a$10$Nnwx70r6csJlAX1.oNgbVOSN8JO5wFZmsubzXlxoXv9eUNlMU6buq'); --密码是‘admin’加密形式
+INSERT INTO `spring_security_test`.`user`(`username`, `password`) VALUES ('admin', '$2a$10$Nnwx70r6csJlAX1.oNgbVOSN8JO5wFZmsubzXlxoXv9eUNlMU6buq'); -- 密码是‘admin’加密形式
 
 -- 开启group时，插入数据
 INSERT INTO `spring_security_test`.`group` (`group_name`) VALUES ('管理员');
